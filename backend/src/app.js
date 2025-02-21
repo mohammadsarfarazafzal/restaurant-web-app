c5f59d599ab82f478f2cffcd258dc24f0cf944f8
Nishant Kumar Bhadani <kumarbhadanin@gmail.com>
2025-02-21 23:26:40 +0530

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,12 +13,12 @@ app.use(cors({
 //middlewares
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public")) 
+
 
 //configuring-parser
 app.use(cookieParser())
 
-app.get('/', function (req, res) {
-  res.send('Server is up and running')
-})
+
 
 export {app}
