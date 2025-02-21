32d19f4229d8ca1650b061bcbb7200abb8b0a4a1
Nishant Kumar Bhadani <kumarbhadanin@gmail.com>
2025-02-21 22:08:08 +0530

    backend

--- backend/src/models/bookTable.models.js
+++ backend/src/models/bookTable.models.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 
-const TableBookingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  name: { type: String, required: true },
-  phone: { type: String, required: true },
-  guests: { type: Number, required: true },
-  date: { type: Date, required: true },
-  time: { type: String, required: true },
-  status: { type: String, enum: ["Pending", "Confirmed", "Cancelled"], default: "Pending" },
-  
-},{timestamps:true});
+const TableBookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    phone: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    guests: { type: Number, required: true },
+    // date: { type: Date, required: true },
+    // time: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
 
 export const TableBooking = mongoose.model("TableBooking", TableBookingSchema);
-
--- backend/src/models/order.models.js
+++ backend/src/models/order.models.js
@@ -1,45 +1,57 @@
 import mongoose from "mongoose";
 import { User } from "./user.models";
 
-const odrerSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      menuItem: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Menu",
-        required: true,
-      },
-      quantity: { type: Number, required: true },
+const odrerSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
+    items: [
+      {
+        menuItem: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Menu",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
 
-  totalPrice: {
-    type: Number,
-    required: true,
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["Pending", "Completed", "Cancelled"],
+      default: "Pending",
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["Paid", "Unpaid"],
+      default: "Unpaid",
+    },
+    orderType: {
+      type: String,
+      enum: ["Dine-in", "Delivery"],
+      required: true,
+    },
+    tableNumber: {
+      type: String,
+      required: function () {
+        return this.orderType === "Dine-in";
+      },
+    },
+    address: {
+      type: String,
+      required: function () {
+        return this.orderType === "Delivery";
+      },
+    },
   },
-  orderStatus: {
-    type: String,
-    enum: ["Pending", "Completed", "Cancelled"],
-    default: "Pending",
-  },
-  paymentStatus: { type: String, enum: ["Paid", "Unpaid"], default: "Unpaid" },
-  orderType:{
-    type:String,
-    enum:["Dine-in","Delivery"],required:true
-  },
-  tableNumber:{
-    type:String,
-    required:function(){
-        return this.orderType==="Dine-in"
-    }
-  },
-  address:{
-    type:String,
-    required:function(){
-        return this.orderType==="Delivery"
-    }
-  }
-},{timestamps:true});
+  { timestamps: true }
+);
 
-export const Order=mongoose.model("Order",odrerSchema)
+export const Order = mongoose.model("Order", odrerSchema);
